4774b52a1d0be45e0f8d175f129fd0e2ffde5125
sujaymanb <sujaymanb@gmail.com>
2018-03-09 16:49:00 -0500

    changes to hover

--- scripts/control.py
+++ scripts/control.py
@@ -12,21 +12,30 @@ from tf.transformations import euler_from_quaternion
 
 class BebopControl():
     def __init__(self):
-        self.speed = 0.02
+        self.speed = 0.1
+        self.min_speed = 0.01
+        self.vert_speed = 0.20
         self.land_initiated = False
         self.angle_zone = 0.10
         self.position_zone = 0.05
+        self.land_zone_base = 0.10
         self.can_rotate = False
+        self.can_descend = False
+        self.angle_locked = False
         self.x_locked = False
         self.y_locked = False
+        self.land_height = 0.5
 
         rospy.init_node('bebop_control', anonymous=False)
 
         #subscribers
+        # !!! Change the callback function in marker subscriber to change landing behavior
+        # !!! > self.marker_callback_simul or > self.marker_callback
+
         rospy.Subscriber('bebop/initiate_landing', Empty, self.land_callback)
-        rospy.Subscriber('/ar_pose_marker', AlvarMarkers, self.marker_callback)
-        #publishers
+        rospy.Subscriber('/ar_pose_marker', AlvarMarkers, self.marker_callback_simul)
         
+        #publishers
         self.pub_takeoff = rospy.Publisher('bebop/takeoff', Empty, queue_size=1, latch=True)
         self.pub_land = rospy.Publisher('bebop/land', Empty, queue_size=1, latch=True)
         self.pub_cmd_vel = rospy.Publisher('bebop/cmd_vel', Twist, queue_size=1)
@@ -53,6 +62,12 @@ class BebopControl():
             rospy.sleep(4)
             rospy.loginfo("> Camera facing down.")
 
+    def non_zero_vel(self, vel_msg):
+        if (vel_msg.linear.x == 0 and vel_msg.linear.y == 0 and vel_msg.linear.z == 0 and vel_msg.angular.z == 0):
+            return False
+
+        return True
+
     # call back for marker pose
     def marker_callback(self, data):
         
@@ -71,12 +86,15 @@ class BebopControl():
                     if yaw < (1.57 - self.angle_zone):
                         print "rotate counter clockwise"
                         vel_msg.angular.z = 0.1
+                        self.angle_locked = False
                     elif yaw > (1.57 + self.angle_zone):
                         print "rotate clockwise"
                         vel_msg.angular.z = -0.1
+                        self.angle_locked = False
                     else:
                         print "angle locked"
                         vel_msg.angular.z = 0
+                        self.angle_locked = True
 
                 # position
 
@@ -90,6 +108,7 @@ class BebopControl():
                 else:
                     vel_msg.linear.y = 0
                     self.x_locked = True
+                    print "x locked"
 
                 # forward-backward
                 if p.y > (0 + self.position_zone):
@@ -101,13 +120,100 @@ class BebopControl():
                 else:
                     vel_msg.linear.x = 0
                     self.y_locked = True
+                    print "y locked"
 
                 if self.x_locked and self.y_locked:
                     self.can_rotate = True
                 else:
                     self.can_rotate = False
 
+                # descend
+                if self.angle_locked == True and self.x_locked == True and self.y_locked == True:
+                    if p.z > self.land_height:
+                        vel_msg.linear.z = -(self.vert_speed)
+                    else:
+                        land = Empty()
+                        self.pub_land.publish(land)
+                        #self.initiate_landing = False
+
                 self.pub_cmd_vel.publish(vel_msg)
+
+    # new call back for marker
+    def marker_callback_simul(self, data):
+        
+        if self.land_initiated:
+            for marker in data.markers:
+                #testing only
+                self.can_rotate = True
+
+                vel_msg = Twist()
+                q = marker.pose.pose.orientation
+                roll,pitch,yaw = euler_from_quaternion([q.x, q.y, q.z, q.w])
+
+                p = marker.pose.pose.position
+                print "position", p
+
+                # rotate
+                if self.can_rotate:
+                    print "euler: ",roll,pitch,yaw
+                    if yaw < (1.57 - self.angle_zone):
+                        print "rotate counter clockwise"
+                        vel_msg.angular.z = 0.05
+                        self.angle_locked = False
+                    elif yaw > (1.57 + self.angle_zone):
+                        print "rotate clockwise"
+                        vel_msg.angular.z = -0.05
+                        self.angle_locked = False
+                    else:
+                        print "angle locked"
+                        vel_msg.angular.z = 0
+                        self.angle_locked = True
+
+                # position
+
+                # left-right
+                if p.x > (0 + self.position_zone):
+                    vel_msg.linear.y = -1 * max(self.speed * abs(p.x), self.min_speed)
+                    #self.x_locked = False
+                elif p.x < (0 - self.position_zone):
+                    vel_msg.linear.y = max(self.speed * abs(p.x), self.min_speed)
+                    #self.x_locked = False
+                else:
+                    vel_msg.linear.y = 0
+                    #self.x_locked = True
+                    print "x locked"
+
+                # forward-backward
+                if p.y > (0 + self.position_zone):
+                    vel_msg.linear.x = -1 * max(self.speed * abs(p.y), self.min_speed)
+                    #self.y_locked = False
+                elif p.y < (0 - self.position_zone):
+                    vel_msg.linear.x = max(self.speed * abs(p.y), self.min_speed)
+                    #self.y_locked = False
+                else:
+                    vel_msg.linear.x = 0
+                    #self.y_locked = True
+                    print "y locked"
+
+                zone = max(self.land_zone_base * p.z, self.position_zone)
+
+                if p.x < (0 + zone) and p.x > (0 - zone) and p.y < (0 + zone) and p.y > (0 - zone):
+                    self.can_descend = True
+                else:
+                    self.can_descend = False
+
+                # descend
+                #if self.angle_locked == True and self.x_locked == True and self.y_locked == True:
+                if self.angle_locked == True and self.can_descend == True:
+                    if p.z > self.land_height:
+                        vel_msg.linear.z = -(self.vert_speed)
+                    else:
+                        land = Empty()
+                        self.pub_land.publish(land)
+                        #self.initiate_landing = False
+
+                if(self.non_zero_vel(vel_msg)):
+                    self.pub_cmd_vel.publish(vel_msg)
 
 if __name__ == '__main__':
     try:
