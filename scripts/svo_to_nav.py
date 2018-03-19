c83813ef44f9d4c706a76c046c947a56db9b1c5b
sujaymanb <sujaymanb@gmail.com>
2018-03-19 12:56:04 -0400

    testing svo for odometry

--- scripts/svo_to_nav.py
+++ scripts/svo_to_nav.py
@@ -1,29 +1,36 @@
 #!/usr/bin/env python
 
 import rospy
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseWithCovarianceStamped
+from geometry_msgs.msg import TwistWithCovariance
 from nav_msgs.msg import Odometry
 
 class NavPublisher():
-    def __init__(self);
-        # TODO: initialize class vars for velocity and covariance
-        
+    def __init__(self):
+        # initialize twist with covariance
+        self.twist = TwistWithCovariance()
 
         rospy.init_node('bebop_svo_to_nav', anonymous=False)
 
         # subscribers (TODO)
-        rospy.Subscriber('svo/pose_cam/0', PoseStamped, self.svo_callback)
         rospy.Subscriber('bebop/odom', Odometry, self.bebop_callback)
+        rospy.Subscriber('svo/pose_imu', PoseWithCovarianceStamped, self.svo_callback)    
 
         # publishers (TODO: publish to /vo for robot pose ekf)
 
-        rospy.pub_vo = rospy.Publisher('/vo', Odometry, queue_size=10)
+        self.pub_vo = rospy.Publisher('/vo', Odometry, queue_size=10)
 
     def svo_callback(self, data):
         # converts to nav_msg odometry type msg and publish
+        odom = Odometry()
+        odom.pose = data.pose
+        odom.twist = self.twist
+
+        self.pub_vo.publish(odom)
 
     def bebop_callback(self, data):
         # gets data for velocity and covariance and store it for svo
+        self.twist = data.twist
 
     def shutdown(self):
         rospy.loginfo("Shutting down bebop_svo_to_nav node")
@@ -31,8 +38,8 @@ class NavPublisher():
 
 if __name__ == '__main__':
     try:
-        rospy.loginfo("Starting bebop_svo_to_nav node.")
         NavPublisher()
+        rospy.loginfo("Starting bebop_svo_to_nav node.")
         rospy.spin()
     except rospy.ROSInterruptException:
         rospy.logerr("Terminating bebop_svo_to_nav node.")
